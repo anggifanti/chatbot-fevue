c8839de3872c73f256e58a9e00403bd66dd17b64
Sachan Adharullyansyah <sachanadharul4@gmail.com>
2025-08-15 11:02:21 +0700

    refactor: enhance icon management by adding oh-vue-icons library and registering components globally

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,96 @@
-import { createApp } from 'vue'
+import { 
+  createApp 
+} from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { useAuthStore } from './stores/auth'
 import './style.css'
 
+// oh-vue-icons configuration
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import {
+  // Material Design icons - using correct names
+  MdChat,
+  MdStar,
+  MdPerson,
+  MdLock,
+  MdDaterange,
+  MdPhotocamera,
+  MdTrendingup,
+  MdSearch,
+  MdHourglasstop,
+  MdAssessment,
+  MdDescription,
+  MdCreate,
+  MdDashboard,
+  MdGroup,
+  MdBarchart,
+  MdDownload,
+  MdPictureaspdf,
+  MdTablechart,
+  MdFavorite,
+  MdThumbup,
+  MdClose,
+  MdAdd,
+  MdRemove,
+  MdEdit,
+  MdDelete,
+  MdVisibility,
+  MdVisibilityoff,
+  MdHome,
+  MdMenu,
+  MdMorevert,
+  MdMorehoriz,
+  MdArrowback,
+  MdArrowforward,
+  MdArrowupward,
+  MdArrowdownward,
+  MdExpandmore,
+  MdExpandless,
+  MdChevronleft,
+  MdChevronright,
+  MdKeyboardarrowup,
+  MdKeyboardarrowdown,
+  MdKeyboardarrowleft,
+  MdKeyboardarrowright,
+  MdSchedule,
+  MdEmail,
+  MdRefresh,
+  MdSettings,
+  MdNotifications,
+  MdExittoapp,
+  MdInsights,
+  MdEmojievents,
+  // New icons for AdminView
+  GiLipstick,
+  MdForum,
+  MdBrush
+} from 'oh-vue-icons/icons'
+
+// Add all icons to the library
+addIcons(
+  MdChat, MdStar, MdPerson, MdLock, MdDaterange, MdPhotocamera,
+  MdTrendingup, MdSearch, MdHourglasstop, MdAssessment,
+  MdDescription, MdCreate, MdDashboard, MdGroup, MdBarchart,
+  MdDownload, MdPictureaspdf, MdTablechart,
+  MdFavorite, MdThumbup, MdClose, MdAdd, MdRemove,
+  MdEdit, MdDelete, MdVisibility, MdVisibilityoff, MdHome, MdMenu,
+  MdMorevert, MdMorehoriz, MdArrowback, MdArrowforward, MdArrowupward,
+  MdArrowdownward, MdExpandmore, MdExpandless, MdChevronleft,
+  MdChevronright, MdKeyboardarrowup, MdKeyboardarrowdown,
+  MdKeyboardarrowleft, MdKeyboardarrowright, MdSchedule,
+  MdEmail, MdRefresh, MdSettings, MdNotifications, MdExittoapp,
+  MdInsights, MdEmojievents,
+  // New icons for AdminView
+  GiLipstick, MdForum, MdBrush
+)
+
 const app = createApp(App)
 const pinia = createPinia()
+
+// Register the oh-vue-icon component globally
+app.component('VIcon', OhVueIcon)
 
 app.use(pinia)
 
